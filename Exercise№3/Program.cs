109e2220a5c33b4aec363822ed49cca18f0e3c70
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-17 22:58:08 +0300

    Exercise№1 completed

---
+++ Exercise№3/Program.cs
@@ -0,0 +1,29 @@
+﻿int fiveDigitNumber = GetNumber("Enter your five-digit number ");
+
+
+int a = fiveDigitNumber / 10000;
+int b = (fiveDigitNumber / 1000) % 10;
+int c = (fiveDigitNumber % 100) / 10;
+int d = fiveDigitNumber % 10;
+if ((fiveDigitNumber.ToString()).Length == 5)
+{
+    if ((a == d) && (b == c))
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {   
+        Console.WriteLine("No");
+    }
+}
+else
+{
+    Console.WriteLine("Please, enter a five-digit number!");
+}
+
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
