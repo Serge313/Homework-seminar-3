332c1da4dea0bd4f77f83b4e00fe7c884e67ec24
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-18 00:07:21 +0300

    Exercise№3 completed

---
+++ Exercise№3/Program.cs
@@ -0,0 +1,21 @@
+﻿int givenNumber = GetNumber("Enter your number ");
+int number = 1;
+
+while (number <= givenNumber)
+{
+    Console.WriteLine(ToThirdPower(number));
+    number++;
+}
+
+
+int ToThirdPower(int a)
+{
+    int thirdPower = (a * a * a);
+    return thirdPower;
+}
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
